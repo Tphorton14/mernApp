4eead769aa13eedfa0c811200d0089665e1f46a3
Tphorton <Tphorton14@gmail.com>
2020-03-13 16:50:12 -0400

    server.js update

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+mongoose.connect('mongodb://localhost/mern_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoooose has been connected!');
+});
+
 // http request logger
 app.use(morgan('tiny'));
 
@@ -29,4 +38,4 @@ app.get('/api', (req, res) => {
 });
 
 // consologging that server is listening --- using backtick
-app.listen(PORT, console.log(`Server is listening at ${PORT}`));
+app.listen(PORT, console.log(`Server is listening on ${PORT}`));
