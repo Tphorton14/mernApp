8ecb72584136e2fc491e87574e7c40761ad429ed
Tphorton <Tphorton14@gmail.com>
2020-03-19 13:01:51 -0400

    creating client side on React

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+
+    //JSX
+    return(
+      <div>
+        <h1>Welcome to my App!!</h1>
+        <form>
+          <div className='form-input'>
+            <input
+            type="text"
+            name="title"
+            placeholder=" Enter Title Here"
+            value=""
+            onChange={2}
+            />
+          </div>
+          <div className='form-input'>
+            <textarea placeholder="Begin typing body here" name="body" cols="30" rows="10" onChange={2}></textarea>
+          </div>
+
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+
 }
+
+
 
 export default App;
