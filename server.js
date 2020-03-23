026151059eafb9d48dccb9744fd1cd543749ee51
Tphorton <Tphorton14@gmail.com>
2020-03-23 08:44:59 -0400

    install/import cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 // built in Node mod
 const path = require('path');
 
